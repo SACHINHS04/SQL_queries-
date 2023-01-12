ae08fae9f8de94362262ad14bfff8ad2164946b6
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-01-13 02:34:14 +0530

    Update SQL_queries_generator_app.py

--- SQL_queries_generator_app.py
+++ SQL_queries_generator_app.py
@@ -1,28 +1,55 @@
 import openai
+
 import streamlit as st
 
-def generate_query():
-    # Get API key from user
-    api_key = st.text_input("Enter your OpenAI API key:")
-    openai.api_key = api_key
+def get_api_key():
 
-    # Get user input
+    api_key = st.text_input("Enter your OpenAI API key:")
+
+    if api_key:
+
+        openai.api_key = api_key
+
+        return True
+
+    else:
+
+        st.error("API key is required.")
+
+        return False
+
+def generate_query():
+
     query_input = st.text_input("Enter a description of the query you want to create:")
-    
-    # Use OpenAI to generate SQL query
+
     prompt = (f"Write a SQL query to {query_input}")
+
     completions = openai.Completion.create(
+
         engine="text-davinci-002",
+
         prompt=prompt,
+
         max_tokens=1024,
+
         n=1,
+
         stop=None,
+
         temperature=0.5,
+
     )
+
     message = completions.choices[0].text
+
     st.success(f"SQL query: {message}")
 
 # Streamlit app
+
 st.set_page_config(page_title="SQL Query Generator", page_icon=":guardsman:", layout="wide")
+
 st.title("SQL Query Generator")
-generate_query()
+
+if get_api_key():
+
+    generate_query()
